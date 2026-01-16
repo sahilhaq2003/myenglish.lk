83820cf88a6ff501bc5bd401410529d7545b2e61
sahilhaq2003 <sahilhaq2003@gmail.com>
2026-01-16 18:15:56 +0530

    update

--- index.tsx
+++ index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './styles/theme.css';
+import './styles/utilities.css';
 
 import { ThemeProvider } from './context/ThemeContext';
 
